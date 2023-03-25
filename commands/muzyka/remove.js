c57e7d6a7551c363be21a4ae2ad8e4665fa93a9a
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-03-25 19:50:27 +0100

    Update remove.js

--- commands/muzyka/remove.js
+++ commands/muzyka/remove.js
@@ -9,22 +9,18 @@ exports.run = async (client, message, args) => {
     
     if (!queue || !queue.isPlaying()) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie gram żadnej piosenki!**`).setColor("Red")]});
 
-    const trackname = queue.tracks[index].title;
-
     if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
 
-    if (index === undefined) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nieprawidłowa liczba!**`).setColor("Red")]});
-
-    if (args[0] <= 0 || args[0] >= queue.tracks.length) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Test!**`).setColor("Red")]});
-
-    if (!trackname) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Ten utwór nie istneje!**`).setColor("Red")]});
+    if (index === undefined || args[0] < 1 || args[0] > queue.tracks.size) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nieprawidłowa liczba!**`).setColor("Red")]});
 
     if (isNaN(index)) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ \`\`${args[0]}\`\` **nie jest liczbą!**`).setColor("Red")]});
 
+    const track = queue.tracks.at(index).title;
+
     try {
-        queue.remove(index);
-        return message.reply({embeds: [new EmbedBuilder().setTitle(`🎯 Usunąłeś piosenkę z kolejki!`).setDescription(`**Usunąłeś:** \`\`${trackname}\`\` `).setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setColor("6b3deb")]});
-    } catch (error) {
+        await queue.node.remove(index);
+        return message.reply({embeds: [new EmbedBuilder().setTitle(`🎯 Usunąłeś piosenkę z kolejki!`).setDescription(`**Usunąłeś:** \`\`${track}\`\` `).setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setColor("6b3deb")]});
+    } catch {
         return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie mogę usunąć tego utworu!**`).setColor("Red")]});
     };
 
