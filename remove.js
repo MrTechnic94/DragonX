bf1452d3bbf5fd830ab122b2694a3d36369cb57b
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-03-25 19:49:35 +0100

    Add files via upload

---
+++ remove.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { EmbedBuilder } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+    const queue = client.player.nodes.get(message.guild.id);
+    const index = args[0] - 1;
+    
+    if (!queue || !queue.isPlaying()) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie gram żadnej piosenki!**`).setColor("Red")]});
+
+    if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
+
+    if (index === undefined || args[0] < 1 || args[0] > queue.tracks.size) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nieprawidłowa liczba!**`).setColor("Red")]});
+
+    if (isNaN(index)) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ \`\`${args[0]}\`\` **nie jest liczbą!**`).setColor("Red")]});
+
+    const track = queue.tracks.at(index).title;
+
+    try {
+        await queue.node.remove(index);
+        return message.reply({embeds: [new EmbedBuilder().setTitle(`🎯 Usunąłeś piosenkę z kolejki!`).setDescription(`**Usunąłeś:** \`\`${track}\`\` `).setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setColor("6b3deb")]});
+    } catch {
+        return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie mogę usunąć tego utworu!**`).setColor("Red")]});
+    };
+
+};
+
+exports.info = {
+    name: "remove"
+}
