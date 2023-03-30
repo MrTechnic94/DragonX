0ef06e48c37d55976d15a12d95496de894b170c6
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-03-30 19:46:29 +0200

    Update loop.js

--- commands/muzyka/loop.js
+++ commands/muzyka/loop.js
@@ -14,7 +14,7 @@ exports.run = async (client, message, args) => {
     if (args[0] === 'off') {
     try {
         if (queue.repeatMode === 0) return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Powtarzanie jest wyłączone!**").setColor("Red")]});
-        if (queue.repeatMode === 1 || queue.repeatMode === 2) await queue.setRepeatMode(QueueRepeatMode.OFF);
+        if (queue.repeatMode === 1 || queue.repeatMode === 2) await queue.setRepeatMode(0);
         return message.reply({embeds: [new EmbedBuilder().setDescription("🔒 **Pętla została zakończona!**").setColor("6b3deb").setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})]});
     } catch {
         return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Nie mogę powtórzyć piosenki!**").setColor("Red")]});
@@ -23,7 +23,7 @@ exports.run = async (client, message, args) => {
     } else if (args[0] === 'track') {
     try {
         if (queue.repeatMode === 1) return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Powtarzanie piosenki jest już włączone!**").setColor("Red")]});
-        if (queue.repeatMode === 0) await queue.setRepeatMode(QueueRepeatMode.TRACK);
+        if (queue.repeatMode === 0) await queue.setRepeatMode(1);
         return message.reply({embeds: [new EmbedBuilder().setTitle("🔂 Powtarzanie piosenek").setDescription(`**Powtarzasz piosenkę:** \`\`${queue.currentTrack.title}\`\``).setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setColor("6b3deb")]});
     } catch {
         return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Nie mogę powtórzyć piosenki!**").setColor("Red")]});
@@ -32,7 +32,7 @@ exports.run = async (client, message, args) => {
     } else if (args[0] === 'queue') {
     try {
         if (queue.repeatMode === 2) return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Powtarzanie kolejki jest już włączone!**").setColor("Red")]});
-        if (queue.repeatMode === 0) await queue.setRepeatMode(QueueRepeatMode.QUEUE);
+        if (queue.repeatMode === 0) await queue.setRepeatMode(2);
         return message.reply({embeds: [new EmbedBuilder().setTitle("🔁 Powtarzanie kolejki").setDescription(`**Powtarzanie kolejki zostało włączone!**`).setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setColor("6b3deb")]});
     } catch {
         return message.reply({embeds: [new EmbedBuilder().setDescription("❌ **Nie mogę powtórzyć piosenki!**").setColor("Red")]});
