7330d000db52612f5a24cce2885c8a2066713ad3
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-03-31 21:13:39 +0200

    Update seek.js

--- commands/muzyka/seek.js
+++ commands/muzyka/seek.js
@@ -6,15 +6,15 @@ exports.run = async (client, message, args) => {
 
     const queue = client.player.nodes.get(message.guild.id);
 
+    const s = parseInt(args[0])
+
     if (!queue || !queue.isPlaying()) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie gram żadnej piosenki!**`).setColor("Red")]});
 
     if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
 
-    if (args[0] * 1000 >= queue.currentTrack.durationMS) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Podana pozycja jest większa od długości utworu, lub równa!**`).setColor("Red")]});
+    if (s * 1000 >= queue.currentTrack.durationMS) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Podany czas jest większa od długości utworu, lub równa!**`).setColor("Red")]});
 
-    if (args[0] === undefined || args[0] <= 0) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nieprawidłowa liczba!**`).setColor("Red")]});
-
-    if (isNaN(args[0])) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ \`\`${args[0]}\`\` **nie jest liczbą!**`).setColor("Red")]});
+    if (!s || s <= 0) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nieprawidłowa liczba!**`).setColor("Red")]});
 
     const time = args[0] * 1000;
 
