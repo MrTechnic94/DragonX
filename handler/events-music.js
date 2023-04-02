2541b004c8502555666fdd127132b1b2cea8b115
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-02 19:32:03 +0200

    Update events-music.js

--- handler/events-music.js
+++ handler/events-music.js
@@ -1,20 +1,17 @@
 'use strict';
 
-module.exports = client => {
+const { readdirSync } = require('fs');
 
-    const { readdirSync } = require('fs');
+module.exports = (client) => {
 
-    const eventload = () => {
-        
-        const events = readdirSync(`./events-music/`).filter(files => files.endsWith(".js"));
-        
-            for(const file of events) {
-                const evn = require(`../events-music/${file}`);
-                console.log((`[`) + "\x1b[36m" + (`Handler`) + "\x1b[0m" + (`]`) + ` Zaladowano wydarzenie ${file}`);
-                client.player.events.on(file.split(".")[0], (...args) => evn.run(client, ...args));
-            
-            }
-    };
+  const eventsDir = './events-music/';
 
-    eventload();
+  const events = readdirSync(eventsDir).filter((file) => file.endsWith('.js'));
+
+  for (const file of events) {
+    const event = require(`../${eventsDir}${file}`);
+    console.log(`[${"\x1b[36m"}Handler${"\x1b[0m"}] Zaladowano wydarzenie ${file}`);
+    client.player.events.on(file.split('.')[0], (...args) => event.run(client, ...args));
+  }
+
 };
