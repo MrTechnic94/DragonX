a072a8385b75fe4a3d723b3db68bca861e489f1b
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-03 19:07:21 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const { ActivityType } = require('discord.js');
-require('dotenv').config({ path: __dirname + '../../.env' });
+require('dotenv').config({ path: `${__dirname}/../../.env` });
 
 exports.run = async (client) => {
     
     const p = process.env.PREFIX;
-
+    
     // -----> Status bota <-----
     client.user.setPresence({ activities: [{ name: "🌙 Connecting...", type: ActivityType.Playing }], status: 'idle' });
-    setTimeout(function() { 
+    setTimeout(() => { 
     client.user.setPresence({ activities: [{ name: `❓ ${p}help 🎵 ${p}play`, type: ActivityType.Listening }], status: 'online' });
     }, 3000);
-    
-    console.log(('[') + "\x1b[31m" + ('Bot') + "\x1b[0m" + (']') + "\x1b[31m" + (` ${client.user.tag} zalogowal sie!`) + "\x1b[0m");    
-    
+
+    console.log(`[\x1b[31mBot\x1b[0m] \x1b[31m${client.user.tag} zalogowal sie!\x1b[0m`);
+
 };
