923348a2d3e20aa92895bbe982061081c86eac79
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-04 19:26:08 +0200

    Update audioTracksAdd.js

--- events-music/audioTracksAdd.js
+++ events-music/audioTracksAdd.js
@@ -5,7 +5,7 @@ const { EmbedBuilder } = require('discord.js');
 exports.run = async (_client, queue) => {
     
     const embed = new EmbedBuilder()
-    .setDescription(`✅ Dodano **${queue.getSize()}** do playlisty!`)
+    .setDescription(`✅ Dodano **${queue.getSize()}** utwory do playlisty!`)
     .setColor("Green")
 
     return queue.metadata.channel.send({embeds: [embed]});
