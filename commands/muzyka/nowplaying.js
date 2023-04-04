e15b2d577703c518b0665929794b5b3a54b715cb
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-04 19:05:34 +0200

    Update nowplaying.js

--- commands/muzyka/nowplaying.js
+++ commands/muzyka/nowplaying.js
@@ -11,28 +11,16 @@ exports.run = async (client, message) => {
     if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
 
     const progresbar = queue.node.createProgressBar({ timecodes: false, length: 13 });
+    const emoji = queue.node.isPaused() ? `▶️` : `⏸️`;
 
-    if (!queue.node.isPaused()) {
     const embed = new EmbedBuilder()
-        .setTitle(`⚡ Teraz Odtwarzam`)
-        .setDescription(`**Tytuł:** ${queue.currentTrack.title}\n**Poziom głośności:** ${queue.node.volume}%\n**Na prośbę:** ${queue.currentTrack.requestedBy}\n\n⏸️ | ${progresbar} ${queue.node.getTimestamp().current.label} / ${queue.currentTrack.duration}`)
-        .setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
-        .setThumbnail(queue.currentTrack.thumbnail)
-        .setColor("6b3deb")
+    .setTitle(`⚡ Teraz Odtwarzam`)
+    .setDescription(`**Tytuł:** ${queue.currentTrack.title}\n**Poziom głośności:** ${queue.node.volume}%\n**Na prośbę:** ${queue.currentTrack.requestedBy}\n\n${emoji} | ${progresbar} ${queue.node.getTimestamp().current.label} / ${queue.currentTrack.duration}`)
+    .setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
+    .setThumbnail(queue.currentTrack.thumbnail)
+    .setColor("6b3deb")
 
-        return message.reply({embeds: [embed]});
-    };
-
-    if (queue.node.isPaused()) {
-    const _embed = new EmbedBuilder()
-        .setTitle(`⚡ Teraz Odtwarzam`)
-        .setDescription(`**Tytuł:** ${queue.currentTrack.title}\n**Poziom głośności:** ${queue.node.volume}%\n**Na prośbę:** ${queue.currentTrack.requestedBy}\n\n▶️ | ${progresbar} ${queue.node.getTimestamp().current.label} / ${queue.currentTrack.duration}`)
-        .setFooter({text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
-        .setThumbnail(queue.currentTrack.thumbnail)
-        .setColor("6b3deb")
-
-        return message.reply({embeds: [_embed]});
-    };
+    return message.reply({embeds: [embed]});
 
 };
 
