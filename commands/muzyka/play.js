202a283ddbedaec98cae179eba7c750d017b2907
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-04 16:08:13 +0200

    Update play.js

--- commands/muzyka/play.js
+++ commands/muzyka/play.js
@@ -4,15 +4,15 @@ const { EmbedBuilder } = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
+    if (!message.member?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na kanale głosowym!**`).setColor("Red")]});
+
+    if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
+
     const res = await client.player.search(args.join(' '), {
         requestedBy: message.member
     });
 
     if (!res?.hasTracks()) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie znaleziono takiej piosenki!**`).setColor("Red")]});
-
-    if (!message.member?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na kanale głosowym!**`).setColor("Red")]});
-
-    if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
 
     const m = await message.channel.send(`🔎 **Proszę czekać wyszukuję...**`);
 
