89bcb075eddc9934ed5536d4bd3d8bef7bd9a165
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-06 19:30:47 +0200

    Update bassboost.js

--- commands/filtry/bassboost.js
+++ commands/filtry/bassboost.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
     switch(args[0]) {
         case 'on':
             if (queue.filters.ffmpeg.isEnabled('bassboost')) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Ten filtr jest już aktywowany!**`).setColor("Red")]});
-            await queue.filters.ffmpeg.toggle(['bassboost', 'normalizer2']);
+            await queue.filters.ffmpeg.toggle(['bassboost', 'normalizer']);
             return message.reply({embeds: [new EmbedBuilder().setDescription(`🎵 **Bassboost został włączony!**`).setFooter({ text: `Użył/a: ${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) }).setColor("Green")]});
 
     case 'off':
