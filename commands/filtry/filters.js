23a43537251c3773c498519cb4516de18dbb45e1
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-06 11:51:49 +0200

    Update filters.js

--- commands/filtry/filters.js
+++ commands/filtry/filters.js
@@ -7,8 +7,6 @@ exports.run = async (client, message) => {
     const queue = client.player.nodes.get(message.guild.id);
 
     if (!queue?.isPlaying()) return message.reply({embeds: [new EmbedBuilder().setTitle('📰 Lista filtrów').setDescription(`🔴 **BassBoostLow**\n🔴 **BassBoost**\n🔴 **BassBoostHigh**\n🔴 **Karaoke**\n🔴 **Nightcore**\n🔴 **Lofi**`).setFooter({text: `Użycie: bassboost <on / off>`}).setColor("6b3deb")]});
-
-    // if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [new EmbedBuilder().setDescription(`❌ **Nie jesteś na moim kanale głosowym!**`).setColor("Red")]});
     
     const bsl = queue.filters.ffmpeg.isEnabled('bassboost_low') ? `🟢 **BassBoostLow**` : `🔴 **BassBoostLow**`;
     const bs = queue.filters.ffmpeg.isEnabled('bassboost') ? `🟢 **BassBoost**` : `🔴 **BassBoost**`;
