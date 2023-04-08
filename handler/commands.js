43ef96c52fb6405aacea2713ec0de9c4db25eeb0
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-08 10:34:57 +0200

    Update commands.js

--- handler/commands.js
+++ handler/commands.js
@@ -13,21 +13,21 @@ module.exports = (client) => {
 
       if (command.run && typeof command.run === 'function') {
         if (client.commands.get(command.info.name)) {
-          console.warn(`[${"\x1b[31m"}Error${"\x1b[0m"}] \x1b[31mZbyt duża ilość komend ma taką samą nazwę: ${command.info.name}!`);
+          console.warn(`[${"\x1b[31m"}Error${"\x1b[0m"}] \x1b[31mZbyt duza ilosc komend ma taką samą nazwe: ${command.info.name}!`);
           continue;
         }
 
         client.commands.set(command.info.name, command);
-        console.log(`[${"\x1b[36m"}Handler${"\x1b[0m"}] Komenda ${command.info.name} została pomyslnie załadowana!`);
+        console.log(`[${"\x1b[36m"}Handler${"\x1b[0m"}] Komenda ${command.info.name} zostala pomyslnie zaladowana!`);
       } else {
-        console.warn(`[[${"\x1b[33m"}Warn${"\x1b[0m"}] \x1b[33mBłąd podczas ładowania komendy ${directory}/${file}!`);
+        console.warn(`[[${"\x1b[33m"}Warn${"\x1b[0m"}] \x1b[33mBlad podczas ladowania komendy ${directory}/${file}!`);
         continue;
       }
 
       if (command.info.aliases && Array.isArray(command.info.aliases)) {
         command.info.aliases.forEach((alias) => {
           if (client.aliases.get(alias)) {
-            console.error(`[${"\x1b[31m"}Error${"\x1b[0m"}] \x1b[31mDwie lub więcej komend posiada takie same aliasy: ${alias}!`);
+            console.error(`[${"\x1b[31m"}Error${"\x1b[0m"}] \x1b[31mDwie lub wiecej komend posiada takie same aliasy: ${alias}!`);
           } else {
             client.aliases.set(alias, command.info.name);
           }
