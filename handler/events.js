2b98a28f54bdb62349599621bf7b3376cecd08e7
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-08 10:29:18 +0200

    Update events.js

--- handler/events.js
+++ handler/events.js
@@ -5,13 +5,11 @@ const path = require('path');
 
 module.exports = (client) => {
 
-    const eventDir = './events/';
-
-    fs.readdirSync(eventDir).forEach((directory) => {
-      const eventFiles = fs.readdirSync(path.join(eventDir, directory)).filter((file) => file.endsWith('.js'));
+    fs.readdirSync(`./events/`).forEach((directory) => {
+      const eventFiles = fs.readdirSync(path.join(`./events/`, directory)).filter((file) => file.endsWith('.js'));
   
       for (const file of eventFiles) {
-        const event = require(path.join(__dirname, '..', eventDir, directory, file));
+        const event = require(path.join(__dirname, '..', `./events/`, directory, file));
         console.log(`[${"\x1b[36m"}Handler${"\x1b[0m"}] Zaladowano wydarzenie ${file}`);
         client.on(file.split('.')[0], (...args) => event.run(client, ...args));
       }
