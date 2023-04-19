73e40da03c1c06ee3ee039f00959e325de6a6d23
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-19 17:26:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const DeezerExtractor = require('discord-player-deezer').default;
 require('dotenv').config();
 
 const client = new Client({
-	messageEditHistoryMaxSize: 0,
-	messageCacheMaxSize: 25,
-	messageSweepInterval: 43200,
-	messageCacheLifetime: 21600,
 	// shards: getInfo().SHARD_LIST,
     // shardCount: getInfo().TOTAL_SHARDS,
+	messageEditHistoryMaxSize: 0,
+	messageCacheMaxSize: 100,
+	messageSweepInterval: 7200,
+	messageCacheLifetime: 86400,
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
@@ -34,10 +34,10 @@ client.player.extractors.register(DeezerExtractor);
 // -----> Zaladowanie discord-hybrid-sharding <-----
 // client.cluster = new ClusterClient(client);
 
-// -----> Zalodowanie handlera <-----
-["commands", "aliases"].forEach(x => (client[x] = new Collection()));
+// -----> Zalodowanie infrastruktury bota <-----
+['commands', 'aliases'].forEach(x => (client[x] = new Collection()));
 
-["./handler/events.js", "./handler/events-music.js", "./handler/commands.js"].forEach(x => require(x)(client));
+['./structures/events.js', './structures/events-music.js', './structures/commands.js'].forEach(x => require(x)(client));
 
 // -----> Zalogowanie bota do discorda <-----
 client.login(process.env.TOKEN);
