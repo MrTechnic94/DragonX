2dc3f0e5f4ee1d193df1178a3dc1e314f4a86dda
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-24 20:43:15 +0200

    Update loop.js

--- commands/muzyka/loop.js
+++ commands/muzyka/loop.js
@@ -11,23 +11,23 @@ exports.run = async (client, message, args) => {
 
     if (message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [embeds.voice_error]});
 
-    switch(args[0]) {
+    switch(args[0].toLowerCase()) {
         case 'off':
         if (queue.repeatMode === QueueRepeatMode.OFF) return message.reply({embeds: [embeds.loop_off_error]});
         if (queue.repeatMode === QueueRepeatMode.TRACK || queue.repeatMode === QueueRepeatMode.QUEUE) await queue.setRepeatMode(QueueRepeatMode.OFF);
-        message.reply({embeds: [new EmbedBuilder().setDescription("🔒 **Pętla została zakończona!**").setColor('Red')]});
+        message.reply({embeds: [new EmbedBuilder().setDescription(`🔒 **Pętla została zakończona!**`).setColor('Red')]});
         break;
         
     case 'track':
         if (queue.repeatMode === QueueRepeatMode.TRACK) return message.reply({embeds: [embeds.loop_track_error]});
         if (queue.repeatMode === QueueRepeatMode.OFF) await queue.setRepeatMode(QueueRepeatMode.TRACK);
-        message.reply({embeds: [new EmbedBuilder().setDescription("🔂 **Powtarzanie piosenki zostało włączone!**").setColor('Red')]});
+        message.reply({embeds: [new EmbedBuilder().setDescription(`🔂 **Powtarzanie piosenki zostało włączone!**`).setColor('Red')]});
         break;
 
     case 'queue':
         if (queue.repeatMode === QueueRepeatMode.QUEUE) return message.reply({embeds: [embeds.loop_queue_error]});
         if (queue.repeatMode === QueueRepeatMode.OFF) await queue.setRepeatMode(QueueRepeatMode.QUEUE);
-        message.reply({embeds: [new EmbedBuilder().setDescription("🔁 **Powtarzanie playlisty zostało włączone!**").setColor('Red')]});
+        message.reply({embeds: [new EmbedBuilder().setDescription(`🔁 **Powtarzanie playlisty zostało włączone!**`).setColor('Red')]});
         break;
     };
 };
