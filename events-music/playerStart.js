d3f0c902ee2677ff58efe4a7a6242e0b050bbdae
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-27 19:35:47 +0200

    Update playerStart.js

--- events-music/playerStart.js
+++ events-music/playerStart.js
@@ -4,16 +4,16 @@ const { EmbedBuilder } = require('discord.js');
 
 exports.run = async (_client, queue, track) => {
     const requester = track.requestedBy ?? `brak`;
-
-    const embed = new EmbedBuilder()
-        .setTitle('▶️ Aktualnie odtwarzam')
-        .setDescription(`[${track.title}](${track.url})`)
-        .addFields(
-            { name: '**Na prośbę:**', value: `${requester}`, inline: true },
-            { name: '**Czas trwania:**', value: `\`\`${track.duration}\`\``, inline: true }
-        )
-        .setThumbnail(track.thumbnail)
-        .setColor('Red')
-
-    queue.npmessage = await queue.metadata.channel.send({embeds: [embed]});
+    
+    queue.npmessage = await queue.metadata.channel.send({
+        embeds: [new EmbedBuilder()
+            .setTitle('▶️ Aktualnie odtwarzam')
+            .setDescription(`[${track.title}](${track.url})`)
+            .addFields(
+                { name: '**Na prośbę:**', value: `${requester}`, inline: true },
+                { name: '**Czas trwania:**', value: `\`\`${track.duration}\`\``, inline: true }
+            )
+            .setThumbnail(track.thumbnail)
+            .setColor('Red')]
+    });
 };
