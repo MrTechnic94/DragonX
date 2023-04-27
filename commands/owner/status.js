3f8e6b57b14126d737d12b09bf5830ce807efe7d
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-27 19:03:53 +0200

    Update status.js

--- commands/owner/status.js
+++ commands/owner/status.js
@@ -6,15 +6,29 @@ const embeds = require('../../utils/embeds.js');
 exports.run = async (client, message, args) => {
     if (!args[0]) return message.reply({embeds: [embeds.args_status_error]});
 
-    client.user.setPresence({activities: [{name: args.join(' '), type: ActivityType.Listening}]});
+    switch(args[0]) {
+        default:
+            client.user.setPresence({activities: [{name: args.join(' '), type: ActivityType.Listening}]});
 
-    return message.reply({
-        embeds: [
-            new EmbedBuilder()
-                .setTitle(`✅ Pomyślnie ustawiono status`)
-                .setDescription(`Status został zmieniony na \`\`${args.join(' ')}\`\``)
-                .setColor('Red')]
-    });
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle(`✅ Pomyślnie ustawiono status`)
+                        .setDescription(`Status został zmieniony na \`\`${args.join(' ')}\`\``)
+                        .setColor('Red')]
+            });
+        
+        case 'clear':
+            client.user.setPresence({activities: [{name: process.env.STATUSTWO, type: ActivityType.Listening}], status: 'online'});
+
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle(`✅ Pomyślnie wyczyszczono status`)
+                        .setDescription(`Status został zmieniony na: \`\`${process.env.STATUSTWO}\`\``)
+                        .setColor('Red')]
+            });
+    };
 };
 
 exports.info = {
