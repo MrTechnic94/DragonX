c18a9c0eda57da8cedfefde63e0a41cf37155704
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-28 18:00:18 +0200

    Update prefix.js

--- commands/info/prefix.js
+++ commands/info/prefix.js
@@ -5,19 +5,23 @@ const GuildSettings = require('../../utils/guildSettings.js');
 const embeds = require('../../utils/embeds.js');
 
 exports.run = async (_client, message, args) => {
-    const prefix = args[0];
+    let prefix = args[0];
 
-    if (!prefix) return message.reply({embeds: [embeds.prefix_change_error]});
+    if (prefix === 'clear') {
+        prefix = process.env.PREFIX;
+    };
 
     let guildData = await GuildSettings.findOne({guildId: message.guild.id});
     const oldPrefix = guildData ? guildData.prefix : process.env.PREFIX;
+
+    if (!prefix) return message.reply({embeds: [embeds.prefix_change_error]});
     
     if (oldPrefix === prefix) return message.reply({embeds: [embeds.already_prefix_error]});
 
     try {
         const guildId = message.guildId;
+
         await GuildSettings.updateOne({guildId}, {guildId, prefix}, {upsert: true});
-      
         return message.reply({embeds: [new EmbedBuilder().setDescription(`✅ **Ustawiono nowy prefix:** \`${prefix}\``).setColor('Red')]});
     } catch {
         return message.reply({embeds: [embeds.prefix_change_error]});
