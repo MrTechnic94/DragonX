4b2649b7282af91d450522ded34cb02214cce09f
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-28 16:21:22 +0200

    Update lyrics.js

--- commands/info/lyrics.js
+++ commands/info/lyrics.js
@@ -5,40 +5,33 @@ const { lyricsExtractor } = require('@discord-player/extractor');
 const embeds = require('../../utils/embeds.js');
 
 exports.run = async (client, message, args) => {
-    const queue = client.player.nodes.get(message.guild.id);
     const lyricsFinder = lyricsExtractor(process.env.LYRICSAPI);
     const query = args.join(' ');
 
-    if (query) return await lyricsFinder.search(query).then(async x => {
+    try {
+        let x;
+        if (query) {
+            x = await lyricsFinder.search(query);
+        } else {
+            const queue = client.player.nodes.get(message.guild.id);
+            if (!queue?.isPlaying()) return message.reply({embeds: [embeds.queue_error]});
+            x = await lyricsFinder.search(queue.currentTrack.title);
+        };
 
-    const embed = new EmbedBuilder()
-        .setTitle(`🎵 ${x.artist.name} - ${x.title}`)
-        .setDescription(`${x.lyrics}`)
-        .setColor('Red')
-
-        return message.reply({embeds: [embed]});    
-
-    }).catch(() => {
+        return message.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle(`📜 ${x.artist.name} - ${x.title}`)
+                    .setDescription(`${x.lyrics}`)
+                    .setColor('Red')
+            ]
+        });
+    } catch {
         return message.reply({embeds: [embeds.lyrics_error]});
-    });
-
-    if (!queue?.isPlaying()) return message.reply({embeds: [embeds.queue_error]});
-
-    if (!query) return await lyricsFinder.search(queue.currentTrack.title).then((x) => {
-
-    const _embed = new EmbedBuilder()
-        .setTitle(`🎵 ${x.artist.name} - ${x.title}`)
-        .setDescription(`${x.lyrics}`)
-        .setColor('Red')
-    
-        return message.reply({embeds: [_embed]});    
-
-    }).catch(() => {
-        return message.reply({embeds: [embeds.lyrics_error]});
-    });
+    };
 };
 
 exports.info = {
     name: "lyrics",
-    aliases: ['ly']
+    aliases: ["ly"]
 };
