fd170747b242d6ee50a01af697530c88b56f2d57
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-04-30 19:51:49 +0200

    Update seek.js

--- commands/muzyka/seek.js
+++ commands/muzyka/seek.js
@@ -7,15 +7,15 @@ exports.run = async (client, message, args) => {
     if (message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [embeds.voice_error]});
 
     const queue = client.player.nodes.get(message.guild.id);
-    const s = parseInt(args[0]);
+    const seekTime = parseInt(args[0]);
 
     if (!queue?.isPlaying()) return message.reply({embeds: [embeds.queue_error]});
 
-    if (s * 1000 >= queue.currentTrack.durationMS) return message.reply({embeds: [embeds.time_seek_error]});
+    if (seekTime * 1000 >= queue.currentTrack.durationMS) return message.reply({embeds: [embeds.time_seek_error]});
 
-    if (!s || s <= 0) return message.reply({embeds: [embeds.number_error]});
+    if (!seekTime || seekTime <= 0) return message.reply({embeds: [embeds.number_error]});
 
-    await queue.node.seek(s * 1000);
+    await queue.node.seek(seekTime * 1000);
     return message.reply({embeds: [createEmbed({description: `🎵 **Ustawiono odtwarzanie na: ${queue.node.getTimestamp().current.label}!**`})]});
 };
 
