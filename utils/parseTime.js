ff0065172b80ac44d16feda2cf210e6bae726307
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-05-02 21:11:00 +0200

    Add files via upload

---
+++ utils/parseTime.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const prettyMs = require('pretty-ms');
+
+function parseTime(timeString) {
+    const timeParts = /^((\d{1,2}):)?(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?([ms])?$/.exec(timeString);
+    if (!timeParts) return null;
+
+    let hours = 0, minutes = 0, seconds = 0, multiplier = 1, milliseconds = 0;
+    if (timeParts[2]) hours = parseInt(timeParts[2], 10);
+    minutes = parseInt(timeParts[3], 10);
+    seconds = parseInt(timeParts[4], 10);
+    if (timeParts[6] === "m") multiplier = 60;
+    else if (timeParts[6] === "s") multiplier = 1 / 60;
+    if (timeParts[5]) milliseconds = prettyMs(`${timeParts[5]}${timeParts[6]}`);
+
+    return ((hours * 3600) + (minutes * 60) + seconds) * 1000 + milliseconds;
+};
+
+module.exports = { parseTime };
