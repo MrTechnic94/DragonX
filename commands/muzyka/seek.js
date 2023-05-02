083213291e0cde17718213be1b893d68ced5d976
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-05-02 21:11:23 +0200

    Update seek.js

--- commands/muzyka/seek.js
+++ commands/muzyka/seek.js
@@ -2,25 +2,30 @@
 
 const embeds = require('../../utils/embeds.js');
 const { createEmbed } = require('../../utils/embedCreator.js');
+const { parseTime } = require('../../utils/parseTime.js');
 
 exports.run = async (client, message, args) => {
-    if (message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [embeds.voice_error]});
+  if (message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.reply({embeds: [embeds.voice_error]});
 
-    const queue = client.player.nodes.get(message.guild.id);
-    const seekTime = parseInt(args[0]);
+  const queue = client.player.nodes.get(message.guild.id);
 
-    if (!queue) return message.reply({embeds: [embeds.queue_error]});
+  if (!queue) return message.reply({embeds: [embeds.queue_error]});
 
-    if (seekTime * 1000 >= queue.currentTrack.durationMS) return message.reply({embeds: [embeds.time_seek_error]});
+  const seekTime = parseTime(args[0]);
+  if (seekTime === null) return message.reply({embeds: [embeds.time_seek_error]});
 
-    if (!seekTime || seekTime <= 0) return message.reply({embeds: [embeds.number_error]});
+  const durationSeconds = Math.round(seekTime / 1000);
 
-    await queue.node.seek(seekTime * 1000);
-    return message.reply({embeds: [createEmbed({description: `🎵 **Ustawiono odtwarzanie na: ${queue.node.getTimestamp().current.label}!**`})]});
+  if (durationSeconds >= queue.currentTrack.duration) return message.reply({embeds: [embeds.time_seek_error]});
+
+  if (durationSeconds <= 0) return message.reply({embeds: [embeds.number_error]});
+
+  await queue.node.seek(durationSeconds * 1000);
+  return message.reply({embeds: [createEmbed({description: `🎵 **Ustawiono odtwarzanie na: ${queue.node.getTimestamp().current.label}!**`})]});
 };
 
 exports.info = {
-    name: "seek",
-    aliases: ["sk"],
-    dj: true
+  name: "seek",
+  aliases: ["sk"],
+  dj: true
 };
