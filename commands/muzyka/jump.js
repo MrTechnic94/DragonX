58d2e864b964e6b2b219e9e70466cd53c2326f41
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-05-09 20:43:55 +0200

    Add files via upload

--- commands/muzyka/jump.js
+++ commands/muzyka/jump.js
@@ -7,10 +7,10 @@ exports.run = async (client, message, args) => {
     if (message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.channel.send({embeds: [embeds.voice_error]});
 
     const queue = client.player.nodes.get(message.guild.id);
-    const index = parseInt(args[0]) - 1;
     
     if (!queue) return message.channel.send({embeds: [embeds.queue_error]});
 
+    const index = parseInt(args[0]) - 1;
     const track = queue.tracks.at(index);
 
     if (!index || !track || index < 0) return message.channel.send({embeds: [embeds.number_error]});
