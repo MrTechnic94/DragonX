da42576fff19f6388e7fae6d5cc3d42895f9acb2
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-05-09 20:55:23 +0200

    Update reply.js

--- commands/muzyka/reply.js
+++ commands/muzyka/reply.js
@@ -4,8 +4,6 @@ const embeds = require('../../utils/embeds.js');
 const { createEmbed } = require('../../utils/embedCreator.js');
 
 exports.run = async (client, message) => {
-    if (!message.member?.voice.channelId) return message.channel.send({embeds: [embeds.member_voice_error]});
-
     if (message.guild.members.me?.voice.channelId && message.member?.voice.channelId !== message.guild.members.me?.voice.channelId) return message.channel.send({embeds: [embeds.voice_error]});
 
     const queue = client.player.nodes.get(message.guild.id);
