fb64783f5e697c85c73f9c7737eada66ec4c4cb4
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2023-06-15 22:10:09 +0200

    Add files via upload

---
+++ info/prefix.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const GuildSettings = require('../../utils/guildSettings.js');
+const { createEmbed } = require('../../utils/embedCreator.js');
+const embeds = require('../../utils/embeds.js');
+
+exports.run = async (_client, message, args) => {
+    try {
+        let prefix = args[0];
+
+        if (prefix === 'clear') prefix = process.env.PREFIX;
+
+        let guildData = await GuildSettings.findOne({ guildId: message.guild.id });
+        const oldPrefix = guildData ? guildData.prefix : process.env.PREFIX;
+
+        if (!prefix) return message.channel.send({ embeds: [embeds.prefix_change_error] });
+
+        if (oldPrefix === prefix) return message.channel.send({ embeds: [embeds.already_prefix_error] });
+
+        const guildId = message.guildId;
+
+        await GuildSettings.updateOne({ guildId }, { guildId, prefix }, { upsert: true });
+
+        return message.channel.send({ embeds: [createEmbed({ description: `✅ **Ustawiono nowy prefix:** \`${prefix}\`` })] });
+    } catch {
+        return message.channel.send({ embeds: [embeds.catch_error] });
+    };
+};
+
+exports.info = {
+    name: "prefix",
+    perm: "Administrator"
+};
