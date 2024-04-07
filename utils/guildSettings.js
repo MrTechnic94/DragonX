8f98eb73a3fb19c4e4534be5a9dc8f49dfb3f2c5
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2024-04-07 22:11:30 +0200

    Update guildSettings.js

--- utils/guildSettings.js
+++ utils/guildSettings.js
@@ -2,6 +2,7 @@
 
 const Redis = require('ioredis');
 
+// Polaczenie sie z baza redis
 const redis = new Redis({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -9,14 +10,17 @@ const redis = new Redis({
 });
 
 module.exports = {
+  // Utworzenie funkcji odpowiadajacej za ustawienie parametrow dla guildi
   async setGuildSettings(guildId, prefix, djRoleId) {
     return await redis.hmset(guildId, 'prefix', prefix, 'djRoleId', djRoleId);
   },
 
+  // Utworzenie funkcji odpowiadajacej za pobranie ustawien guildi
   async getGuildSettings(guildId) {
     return await redis.hgetall(guildId);
   },
 
+  // Utworzenie funkcji odpowiadajacej za usuwanie parametrow z guildi
   async removeGuildSettings(guildId) {
     return await redis.del(guildId);
   }
