41a282defacb51f68c55260959e1e523faadff9b
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2024-04-13 21:20:48 +0200

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const redis = require('../../utils/redis.js');
 const logger = require('../../utils/consoleLogger.js');
 const { Events } = require('discord.js');
 
@@ -8,16 +7,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     run: async (client) => {
-        // Obsluga zdarzen zwiazanych z polaczeniem i bledami bazy danych
-        redis.once('connect', () => {
-            logger.info(`Polaczono z baza danych!`);
-        });
-
-        redis.on('error', (err) => {
-            logger.error(`Blad podczas laczenia z baza danych!\n${err}`);
-            process.exit(1);
-        });
-
         // Wyswietlenie informacji o zalogowaniu sie bota
         logger.success(`${client.user.tag} zalogowal sie!`);
 
--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const Redis = require('ioredis');
+const logger = require('./consoleLogger.js');
 
 // Utworzenie klienta redis
 const redis = new Redis({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD
+});
+
+logger.info(`Polaczono z baza danych!`);
+
+// Obsluga zdarzenia zwiazana z bledami bazy danych
+redis.on('error', (err) => {
+    logger.error(`Blad podczas laczenia z baza danych!\n${err}`);
+    process.exit(1);
 });
 
 module.exports = redis;
