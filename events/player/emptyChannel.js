e1e9b6f76f63c4ca7af0feaebe74274aeb3c098c
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2024-04-22 15:18:52 +0200

    Delete events/player/emptyChannel.js

--- events/player/emptyChannel.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    name: 'emptyChannel',
-    run: async (_client, queue) => {
-        if (queue.npmessage?.editable) await queue.npmessage.delete().catch(() => { });
-    }
-};
