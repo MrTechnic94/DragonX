dee4e5f291563c72f596f0bf5d5462e512284083
MrTechnic94 <101214823+MrTechnic94@users.noreply.github.com>
2024-04-29 20:25:28 +0200

    Update consoleLogger.js

--- utils/consoleLogger.js
+++ utils/consoleLogger.js
@@ -1,16 +1,22 @@
 'use strict';
 
 const pino = require('pino');
+let logger;
 
-// Utworzenie zmiennej logger z ustawieniami pino-pretty
-const logger = pino({
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:HH:MM:ss'
+try {
+    // Sprawdzenie, czy modul pino-pretty jest dostepny, jesli jest dostepny to go uzyje
+    logger = pino({
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'SYS:HH:MM:ss'
+            }
         }
-    }
-});
+    });
+} catch {
+    // Uzycie zwyklego pino, jesli modul nie istnieje lub wystapil blad
+    logger = pino();
+};
 
 module.exports = logger;
